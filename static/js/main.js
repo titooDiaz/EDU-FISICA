a8834b99c673cf81039d168ed48f06da2166090c
Miguel Diaz <107213851+titooUvU@users.noreply.github.com>
2023-06-02 22:42:11 -0500

    Finish!

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+/* ========== Navigation =========== */
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".nav-list .close");
+const menu = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+  menu.classList.remove("show");
+});
+
+/* ========== SignIn Form =========== */
+const signInForm = document.querySelector("header .wrapper");
+
+document.querySelector(".signin").onclick = () => {
+  signInForm.classList.add("active");
+};
+
+document.querySelector(".close-form").onclick = () => {
+  signInForm.classList.remove("active");
+};
